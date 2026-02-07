7b8e8d77eb7bfa10429f7a0c3c5b6d68c3a1bc7d
MIKIK1003 <michel.stegmann@outlook.com>
2026-02-07 11:40:43 +0100

    Neustart für zweite Seite Dashboard

---
+++ MyCoinFlow/Models/GroupingOption.cs
@@ -0,0 +1,16 @@
+﻿namespace MyCoinFlow.Models
+{
+    public class GroupingOption
+    {
+        public string Label { get; }
+        public string Key { get; }
+
+        public GroupingOption(string label, string key)
+        {
+            Label = label;
+            Key = key;
+        }
+
+        public override string ToString() => Label;
+    }
+}
